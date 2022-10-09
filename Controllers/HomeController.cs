f997d7903152996b105629ca3bf35162a1c5ab18
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-09 20:44:34 +0200

    Changed the root [Route("/")]

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,9 @@ namespace API_Interactive_Lab_1.Controllers
     [ApiController]
     //use the name of the controller itself
     //[Route("[controller]")]
-    [Route("Noodles")]
+    //[Route("Noodles")]
+    //[Route("")]
+    [Route("/")]
     public class HomeController : ControllerBase
     {
         [HttpGet]
