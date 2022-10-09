dc93bad8730380c80edf393d3a2677fd8304d37f
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-09 22:17:12 +0200

    CoffeeControllerTests implementation

--- Tests/Controllers/CoffeeControllerTests.cs
+++ Tests/Controllers/CoffeeControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using API_Interactive_Lab_1.Controllers;
+using API_Interactive_Lab_1.Model;
 using NUnit.Framework;
 
 namespace API_Interactive_Lab_1.Tests.Controllers
@@ -7,17 +8,39 @@ namespace API_Interactive_Lab_1.Tests.Controllers
     {
         // this attribute tells NUnit to run this method as a test
         [Test]
-        public void TestGetCoffee()
+        public void TestGetCoffeeLover()
         {
             // ARRANGE - we set up everything needed for our test
             string expectedContent = "I like coffee!";
-
             var controller = new CoffeeController();
             // ACT - we perform the test
-            var result = controller.Get();
+            var result = controller.GetLover();
             // ASSERT - we compare the result to the expected result
-
             Assert.AreEqual(expectedContent, result);
+        }
+        [Test]
+        public void TestGetCoffeeNoParams()
+        {
+            Coffee expected = new Coffee(-1, "Latte");
+
+            var controller = new CoffeeController();
+            var result = controller.Get(null);
+
+            Assert.AreEqual(expected.Name, result.Name);
+            Assert.AreEqual(expected.Id, result.Id);
+        }
+        [Test]
+        public void TestGetCoffeeByName()
+        {
+            string name = "LatteCaffee";
+            var controller = new CoffeeController();
+            var result = controller.Get(name);
+
+            int id = result.Id;
+            Coffee expected = new Coffee(id, name);
+
+            Assert.AreEqual(expected.Name, result.Name);
+            Assert.AreEqual(expected.Id, result.Id);
         }
     }
 }
