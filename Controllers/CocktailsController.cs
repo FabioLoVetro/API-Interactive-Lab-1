9ffa8405bb34c1d5bbf240380efd850c4eacaf75
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-09 22:28:38 +0200

    CocktailsController implementation

---
+++ Controllers/CocktailsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Interactive_Lab_1.Controllers
+{
+    [ApiController]
+    //use the name of the controller itself
+    //[Route("Noodles")]
+    //[Route("[controller]")]
+    //[Route("")]
+    [Route("cocktails")]
+    public class CocktailsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "It's time to drink something";
+        }
+    }
+}
