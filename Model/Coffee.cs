c806cddd70ec4f90dc878494a11c4b7568fb7491
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-09 22:17:38 +0200

    Coffee model implementation

--- Model/Coffee.cs
+++ Model/Coffee.cs
@@ -2,5 +2,12 @@
 {
     public class Coffee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coffee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
